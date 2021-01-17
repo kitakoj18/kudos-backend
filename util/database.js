cc1e6c71a2c468a0934c08a9f718b158d025bade
kitakoj18 <kitakoj18@gmail.com>
2021-01-17 09:25:56 -0800

    added condition to check if in dev or prod env to set dev or prod database variables

--- util/database.js
+++ util/database.js
@@ -1,14 +1,29 @@
 const Sequelize = require('sequelize');
 
 const dotenv = require('dotenv').config();
-const aws_db_pw = process.env.AWS_DB_PW;
-const aws_db_host = process.env.AWS_DB_HOST;
-const aws_db_port = process.env.AWS_DB_PORT;
 
-const sequelize = new Sequelize('kudos', 'root', aws_db_pw, {
+let db_pw;
+let db_host;
+let db_port;
+
+if(process.env.NODE_ENV === 'production'){
+    db_pw = process.env.AWS_DB_PW;
+    db_host = process.env.AWS_DB_HOST;
+    db_port = parseInt(process.env.AWS_DB_PORT);
+} else{
+    db_pw = process.env.LOCAL_DB_PW;
+    db_host = 'localhost';
+    db_port = 3306;
+}
+
+// const aws_db_pw = process.env.AWS_DB_PW;
+// const aws_db_host = process.env.AWS_DB_HOST;
+// const aws_db_port = process.env.AWS_DB_PORT;
+
+const sequelize = new Sequelize('kudos', 'root', db_pw, {
     dialect: 'mysql', 
-    host: aws_db_host,
-    port: parseInt(aws_db_port)
+    host: db_host,
+    port: db_port
 });
 
 module.exports = sequelize; 
