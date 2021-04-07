827db3c266c7de160d6c63c8b0e29a0e0d2a4eb9
kitakoj18 <kitakoj18@gmail.com>
2021-04-06 21:59:42 -0700

    edited up function to populate transaction classId column

--- migrations/20210407030119-populate-classID.js
+++ migrations/20210407030119-populate-classID.js
@@ -7,16 +7,18 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     const t = await queryInterface.sequelize.transaction()
     try {
+      
       const transactions = await Transaction.findAll()
       for await (const transaction of transactions){
-        const studentId = await transaction.studentId
+        console.log(transaction.toJSON())
+        const studentId = await transaction.toJSON().studentId
         const student = await Student.findByPk(studentId)
 
         await transaction.update({ classId: student.classId })
       }
 
       await t.commit()
-      
+
     } catch(err){
       await t.rollback()
       console.log("something went wrong; transaction classId did not populate")
