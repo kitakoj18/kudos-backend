09fb9feb655fe5dfed816498fb6016c445cbb2b0
kitakoj18 <kitakoj18@gmail.com>
2021-04-11 20:43:17 -0600

    changed auth function to use apollo-sereer authenticationerror

--- util/errors.js
+++ util/errors.js
@@ -1,14 +1,12 @@
+const { AuthenticationError } = require('apollo-server')
+
 exports.checkAuth = (req, userTypeSign, userType) =>{
     if(!req.isAuth){
-        const error = new Error('Not authenticated!');
-        error.code = 401;
-        throw error;
+        throw new AuthenticationError('Not authenticated!')
     }
 
     if(req.userType !== userTypeSign){
-        const error = new Error(`Sorry, you must be a ${userType} to access this page or do this action!`);
-        error.code = 401;
-        throw error;
+        throw new AuthenticationError(`Sorry, you must be a ${userType} to access this page or do this action!`)
     }
 }
 
