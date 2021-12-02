e42d271749868047ba58ab12a4d05e19b6fb6436
kitakoj18 <kitakoj18@gmail.com>
2021-12-02 15:23:01 -0800

    Adjusted access token expiration date if in development or prod.

--- util/tokens.js
+++ util/tokens.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const dotenv = require('dotenv').config();
+const isProd = process.env.PORT;
 const accessTokenSignature = process.env.A_JWT_SIGNATURE;
 const refreshTokenSignature = process.env.R_JWT_SIGNATURE;
 
@@ -9,7 +10,7 @@ exports.createAccessToken = ( userId, userType, classId ) =>{
         userId,
         userType,
         classId
-    }, accessTokenSignature, { expiresIn: '15m' })
+    }, accessTokenSignature, { expiresIn: isProd ? '15m' : '1h'})
 }
 
 exports.createRefreshToken = ( userId, userType, classId ) =>{
